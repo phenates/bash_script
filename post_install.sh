ab40207bce97fadc9dd38ed288b9c73fcd7072e5
phenates <phen4tes@gmail.com>
2024-06-14 18:03:39 +0200

    wip

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 #
-# Phenates; v0.4
-# Postinstall script, configure bashrc file for user and root, install package.
-# Copy and unzip bash_script directory from github (wget https://github.com/phenates/bash_script/archive/refs/heads/master.zip)
-#  sh -c "$(wget https://raw.githubusercontent.com/phenates/bash_script/master/post_install.sh)"
+# Phenates; v0.5
+# Postinstall script: packages installation, zsh shell & oh-my-zsh instalation, dotfiles management by chezmoi.
+# Run: bash -c "$(wget https://raw.githubusercontent.com/phenates/bash_script/master/post_install.sh)" -i
 
 #Variables:
 ME=$(basename "$0")
@@ -123,140 +122,6 @@ root_check() {
 }
 
 #######################################
-# User prompt .bashrc file configuration
-#######################################
-user_bashrc_conf() {
-  echo_step "Configuration of $USER .bashrc file:"
-  echo_ask "Continue [y]/[n] ?"
-  case $REPLY in
-  [yY])
-    # Test .bashrc or .bashrc.bak file exist
-    if [[ ! -f "$HOME/.bashrc" || -f "$HOME/.bashrc.bak" ]]; then
-      echo_failure "No .bashrc or existing .bashrc.bak file backup found in $HOME."
-      return 1
-    fi
-
-    # Backup previous .bashrc file
-    echo_step_info "Backup previous .bashrc file to $HOME/.bashrc.bak"
-    cp "$HOME/.bashrc" "$HOME/.bashrc.bak"
-    echo_success
-
-    # Update .bashrc file
-    echo_step_info "Updating current .bashrc file"
-    sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' "$HOME/.bashrc"
-    {
-      echo ""
-      echo ""
-      echo "# Custom part.:"
-      echo "export PATH='$HOME/bash_script:$PATH'"
-      echo "bind -s 'set completion-ignore-case on'"
-      # shellcheck disable=SC2028
-      echo 'PS1="\[\e[38;5;34m\]\u\[\e[38;5;244m\]@\[\e[38;5;214m\]\h \[\e[38;5;129m\]\w \[\033[0m\]$ "'
-      echo "alias ..='cd ..'"
-      echo "alias ll='ls -lah'"
-      echo "alias sys='systemctl'"
-    } >>"$HOME/.bashrc"
-    echo_success "!!! To update prompt, use command: 'source .bashrc' or reconnect user"
-    ;;
-  [nN])
-    echo_canceled "by user"
-    return 1
-    ;;
-  *)
-    echo_failure "Please answer y or n."
-    return 1
-    ;;
-  esac
-}
-
-#######################################
-# root prompt .bashrc file configuration
-#######################################
-# function root_bashrc_conf() {
-#   echo_step "Configuration of root .bashrc file:"
-#   echo_ask "Continue [y]/[n] ?"
-#   case $REPLY in
-#   [yY])
-#     Test .bashrc or .bashrc.bak file exist
-#     if [[ ! -e "/root/.bashrc" || -e "/root/.bashrc.bak" ]]; then
-#       echo_failure "No .bashrc or existing .bashrc.bak backup file found in /root."
-#       return 1
-#     fi
-
-#     # Backup previous .bashrc file
-#     echo_step_info "Backup previous .bashrc file to /root/.bashrc.bak."
-#     cp "/root/.bashrc" "/root/.bashrc.bak"
-#     echo_success
-
-#     # Update .bashrc file
-#     echo_step_info "Updating current .bashrc file"
-#     {
-#       echo ""
-#       echo ""
-#       echo "# Custom part.:"
-#       # echo "export PATH='$HOME/bash_script:$PATH'"
-#       echo "force_color_prompt=yes"
-#       echo "bind -s 'set completion-ignore-case on'"
-#       # shellcheck disable=SC2028
-#       echo 'PS1="\[\e[38;5;160m\]\u\[\e[38;5;244m\]@\[\e[38;5;214m\]\h \[\e[38;5;129m\]\w \[\033[0m\]$ "'
-#       echo "alias ..='cd ..'"
-#       echo "alias ll='ls -lah'"
-#       echo "alias sys='systemctl'"
-#     } >>"$HOME/.bashrc"
-#     echo_success
-#   ;;
-#   [nN])
-#     echo_canceled "by user"
-#     return 1
-#     ;;
-#   *)
-#     echo_failure "Please answer y or n."
-#     return 1
-#     ;;
-#   esac
-# }
-
-#######################################
-# User .nanorc file configuration
-#######################################
-user_nano_conf() {
-  echo_step "Configuration of .nanorc file for $USER:"
-  echo_ask "Continue [y]/[n] ?"
-  case $REPLY in
-  [yY])
-    # Test .nanorc or .nanorc.bak file exist
-    if [[ -f "$HOME/.nanorc" ]]; then
-      echo_failure ".nanorc file already existing."
-      return 1
-    fi
-
-    # Create .nanorc file based on /etc/nanorc
-    echo_step_info "Create $HOME/.nanorc based on /etc/nanorc"
-    cp /etc/nanorc "$HOME/.nanorc"
-    echo_success
-
-    # Update .bashrc file
-    echo_step_info "Updating $HOME/.nanorc file"
-    sed -i 's/# set autoindent/set autoindent/g' "$HOME/.nanorc"
-    sed -i 's/# set indicator/set indicator/g' "$HOME/.nanorc"
-    sed -i 's/# set positionlog/set positionlog/g' "$HOME/.nanorc"
-    {
-      echo include "/usr/share/nano/*.nanorc"
-    } >>"$HOME/.nanorc"
-    echo_success
-    ;;
-  [nN])
-    echo_canceled "by user"
-    return 1
-    ;;
-  *)
-    echo_failure "Please answer y or n."
-    return 1
-    ;;
-  esac
-}
-
-#######################################
 # Packages installation from $PACKAGES.
 #######################################
 package_inst() {
@@ -272,8 +137,8 @@ package_inst() {
     for i in "${PACKAGES[@]}"; do
       echo_step_info "$i install"
       sudo apt install "$i" -y
+      echo_success
     done
-    echo_success
     ;;
   [nN])
     echo_canceled "by user"
@@ -290,12 +155,12 @@ package_inst() {
 # zsh shell and ohmyzsh installation.
 #######################################
 zsh_inst() {
-  echo_step "zsh & ohmyzsh installation:"
+  echo_step "zsh & oh-my-zsh installation:"
   echo_ask "Continue [y]/[n] ?"
   case $REPLY in
   [yY])
     if [[ ! $(zsh --version) ]]; then
-      echo_step_info "Install chezmoi package"
+      echo_step_info "zsh package"
       sudo apt install zsh fonts-powerline
       echo_success
     else
@@ -304,7 +169,7 @@ zsh_inst() {
     fi
 
     if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-      echo_step_info "Install ohmyzsh"
+      echo_step_info "Install oh-my-zsh"
       sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
       echo_success
     else
@@ -360,6 +225,93 @@ dotfiles_inst() {
 }
 
 #######################################
+# User prompt .bashrc file configuration
+#######################################
+# user_bashrc_conf() {
+#   echo_step "Configuration of $USER .bashrc file:"
+#   echo_ask "Continue [y]/[n] ?"
+#   case $REPLY in
+#   [yY])
+#     # Test .bashrc or .bashrc.bak file exist
+#     if [[ ! -f "$HOME/.bashrc" || -f "$HOME/.bashrc.bak" ]]; then
+#       echo_failure "No .bashrc or existing .bashrc.bak file backup found in $HOME."
+#       return 1
+#     fi
+
+#     # Backup previous .bashrc file
+#     echo_step_info "Backup previous .bashrc file to $HOME/.bashrc.bak"
+#     cp "$HOME/.bashrc" "$HOME/.bashrc.bak"
+#     echo_success
+
+#     # Update .bashrc file
+#     echo_step_info "Updating current .bashrc file"
+#     sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' "$HOME/.bashrc"
+#     {
+#       echo ""
+#       echo ""
+#       echo "# Custom part.:"
+#       echo "export PATH='$HOME/bash_script:$PATH'"
+#       echo "bind -s 'set completion-ignore-case on'"
+#       # shellcheck disable=SC2028
+#       echo 'PS1="\[\e[38;5;34m\]\u\[\e[38;5;244m\]@\[\e[38;5;214m\]\h \[\e[38;5;129m\]\w \[\033[0m\]$ "'
+#       echo "alias ..='cd ..'"
+#       echo "alias ll='ls -lah'"
+#       echo "alias sys='systemctl'"
+#     } >>"$HOME/.bashrc"
+#     echo_success "!!! To update prompt, use command: 'source .bashrc' or reconnect user"
+#     ;;
+#   [nN])
+#     echo_canceled "by user"
+#     return 1
+#     ;;
+#   *)
+#     echo_failure "Please answer y or n."
+#     return 1
+#     ;;
+#   esac
+# }
+
+#######################################
+# User .nanorc file configuration
+#######################################
+# user_nano_conf() {
+#   echo_step "Configuration of .nanorc file for $USER:"
+#   echo_ask "Continue [y]/[n] ?"
+#   case $REPLY in
+#   [yY])
+#     # Test .nanorc or .nanorc.bak file exist
+#     if [[ -f "$HOME/.nanorc" ]]; then
+#       echo_failure ".nanorc file already existing."
+#       return 1
+#     fi
+
+#     # Create .nanorc file based on /etc/nanorc
+#     echo_step_info "Create $HOME/.nanorc based on /etc/nanorc"
+#     cp /etc/nanorc "$HOME/.nanorc"
+#     echo_success
+
+#     # Update .bashrc file
+#     echo_step_info "Updating $HOME/.nanorc file"
+#     sed -i 's/# set autoindent/set autoindent/g' "$HOME/.nanorc"
+#     sed -i 's/# set indicator/set indicator/g' "$HOME/.nanorc"
+#     sed -i 's/# set positionlog/set positionlog/g' "$HOME/.nanorc"
+#     {
+#       echo include "/usr/share/nano/*.nanorc"
+#     } >>"$HOME/.nanorc"
+#     echo_success
+#     ;;
+#   [nN])
+#     echo_canceled "by user"
+#     return 1
+#     ;;
+#   *)
+#     echo_failure "Please answer y or n."
+#     return 1
+#     ;;
+#   esac
+# }
+
+#######################################
 # Import & copy some script.
 #######################################
 script_import() {
@@ -395,35 +347,35 @@ script_import() {
 #######################################
 # Uninstall script, remove packages & .bashrc modification.
 #######################################
-remove() {
-  echo_step "Restore .bashrc file"
-  if [[ -f "$HOME/.bashrc.bak" ]]; then
-    cp -vf "$HOME/.bashrc.bak" "$HOME/.bashrc"
-    rm "$HOME/.bashrc.bak"
-    echo_success
-  else
-    echo_failure "No $HOME/.bashrc.bak file found, .bashrc restoration canceled"
-  fi
+# remove() {
+#   echo_step "Restore .bashrc file"
+#   if [[ -f "$HOME/.bashrc.bak" ]]; then
+#     cp -vf "$HOME/.bashrc.bak" "$HOME/.bashrc"
+#     rm "$HOME/.bashrc.bak"
+#     echo_success
+#   else
+#     echo_failure "No $HOME/.bashrc.bak file found, .bashrc restoration canceled"
+#   fi
 
-  if [[ -f "$HOME/.nanorc" ]]; then
-    echo_step "Restore nano configuration"
-    rm "$HOME/.nanorc"
-    echo_success
-  fi
+#   if [[ -f "$HOME/.nanorc" ]]; then
+#     echo_step "Restore nano configuration"
+#     rm "$HOME/.nanorc"
+#     echo_success
+#   fi
 
-  for i in "${PACKAGES[@]}"; do
-    echo_step_info "Uninstall $i package"
-    sudo apt remove "$i" -y
-    echo_success
-  done
+#   for i in "${PACKAGES[@]}"; do
+#     echo_step_info "Uninstall $i package"
+#     sudo apt remove "$i" -y
+#     echo_success
+#   done
 
-  if [[ -d "$HOME/bash_script" ]]; then
-    echo_step "Delete $HOME/bash_script directory"
-    rm -r "$HOME/bash_script"
-    echo_success
-  fi
+#   if [[ -d "$HOME/bash_script" ]]; then
+#     echo_step "Delete $HOME/bash_script directory"
+#     rm -r "$HOME/bash_script"
+#     echo_success
+#   fi
 
-}
+# }
 
 #######################################
 # Main function
@@ -436,20 +388,15 @@ main() {
     ;;
   -i | --install)
     sudo_check
-    user_bashrc_conf
-    # root_bashrc_conf
-    user_nano_conf
     package_inst
-    script_import
+    zsh_inst
+    dotfiles_inst
+    echo_step "Log out and log back in again to use your new default shell..."
     ;;
   -r | --remove)
     remove
     ;;
-  -t)
-    package_inst
-    zsh_inst
-    dotfiles_inst
-    ;;
+  -t) ;;
   *)
     usage
     ;;
